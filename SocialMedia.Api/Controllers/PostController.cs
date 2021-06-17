28b0100ea772ae9471d981eb32c584940841c767
lcalisaya <calisaya.lucia@gmail.com>
2021-06-17 18:18:52 -0300

    Se completa una primera api solicitando una lista de posts

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infrastructure.Repositories;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPost() 
+        {
+            var posts = new PostRepository().GetPosts();
+            //Retorna un status 200
+            return Ok(posts);
+        }
+
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,25 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            //Para simular una consulta a la BBDD
+            var posts = Enumerable.Range(1, 10).Select(x => new Post 
+            { 
+                PostId = x,
+                UserId = x * 2,
+                Description = $"Descripción del post Nº {x}",
+                Date = DateTime.Now,
+                Image = $"http://imageexample.com/{x}"
+            });
+
+            return posts;
+        }
+    }
+}
