897735e9fd802d813237fcd99bec3137e9dd6ef2
lcalisaya <calisaya.lucia@gmail.com>
2021-06-18 18:12:56 -0300

    Se emplea el contexto SocialMediaContext para traer los datos de la BBDD

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Data;
 using SocialMedia.Infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
@@ -29,9 +31,15 @@ namespace SocialMedia.Api
         {
             services.AddControllers();
 
+            //Para indicar que el contexto se basa en la connection string SocialMedia
+            services.AddDbContext<SocialMediaContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SocialMedia"))
+            );
+
             //Para resolver dependencias
             //Cada vez que se llame a esta abstracción, se creará una instancia de esta clase
             services.AddTransient<IPostRepository, PostRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
