43c87392c2159927fa31e047f40b84c6e483dc02
lcalisaya <calisaya.lucia@gmail.com>
2021-06-18 17:40:54 -0300

    Se llevan las entidades tablas al proyecto Core

--- SocialMedia.Infrastructure/Data/SocialMediaContext.cs
+++ SocialMedia.Infrastructure/Data/SocialMediaContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using SocialMedia.Core.Entities;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -22,15 +23,6 @@ namespace SocialMedia.Infrastructure.Data
         public virtual DbSet<Comentario> Comentario { get; set; }
         public virtual DbSet<Publicacion> Publicacion { get; set; }
         public virtual DbSet<Usuario> Usuario { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SocialMedia;Integrated Security = true");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -107,8 +99,6 @@ namespace SocialMedia.Infrastructure.Data
                     .HasMaxLength(10)
                     .IsUnicode(false);
             });
-
-            OnModelCreatingPartial(modelBuilder);
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
